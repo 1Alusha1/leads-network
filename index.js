f8c5eb3d730879ba9b05071fd204187344109b2e
1alusha1 <alusha9636@gmail.com>
2025-04-16 14:32:42 +0200

    deleted useless file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { google } = require('googleapis');
-const cfg = require('./cfg.js');
 const fs = require('fs');
 const path = require('path');
 const app = express();
