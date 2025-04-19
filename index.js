32390be25cc618a9a5dbd85249112e018bfefae2
1alusha1 <alusha9636@gmail.com>
2025-04-19 03:39:32 +0300

    update

--- index.js
+++ index.js
@@ -5,37 +5,15 @@ const format = require('date-format');
 const dotenv = require('dotenv');
 dotenv.config();
 
-async function appendToSheet(data) {
-  const auth = new google.auth.GoogleAuth({
-    credentials: JSON.parse(process.env.GOOGLE_CRED),
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  });
-
-  const client = await auth.getClient();
-  const sheets = google.sheets({ version: 'v4', auth: client });
-
-  const spreadsheetId = '1Fsknq-JhWjUy7RH6bTVPkdcXFbFldIZz2zbyesS6wc8';
-  const range = 'leads';
-
-  await sheets.spreadsheets.values.append({
-    spreadsheetId,
-    range,
-    valueInputOption: 'USER_ENTERED',
-    requestBody: {
-      values: [data],
-    },
-  });
-
-  console.log('✅ Данные успешно добавлены!');
-}
+async function appendToSheet(data) {}
 
 function base64ToString(base64) {
   return decodeURIComponent(escape(atob(base64)));
 }
 
-app.get('/',(req,res)=>{
-  res.send('hello')
-})
+app.get('/', (req, res) => {
+  res.send('hello');
+});
 
 app.get('/record', async (req, res) => {
   try {
@@ -44,20 +22,36 @@ app.get('/record', async (req, res) => {
 
     const decodedPayload = base64ToString(payload);
     const [ip, advertisment, pixel, geo] = decodedPayload.split('&');
-    const recordData = [];
 
-    recordData.push(
-      username,
-      fullname,
-      userId,
-      advertisment,
-      pixel,
-      ip,
-      geo,
-      format('dd-MM-yyyy, hh:mm')
-    );
+    const auth = new google.auth.GoogleAuth({
+      credentials: JSON.parse(process.env.GOOGLE_CRED),
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
 
-    appendToSheet(recordData);
+    const client = await auth.getClient();
+    const sheets = google.sheets({ version: 'v4', auth: client });
+
+    const spreadsheetId = '1Fsknq-JhWjUy7RH6bTVPkdcXFbFldIZz2zbyesS6wc8';
+    const range = 'leads';
+
+    await sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range,
+      valueInputOption: 'USER_ENTERED',
+      requestBody: {
+        values: [
+          username,
+          fullname,
+          userId,
+          advertisment,
+          pixel,
+          ip,
+          geo,
+          format('dd-MM-yyyy, hh:mm'),
+        ],
+      },
+    });
+
     res.status(200).send('✅ Записано в таблицу');
   } catch (err) {
     console.error('❌ Ошибка в маршруте:', err);
