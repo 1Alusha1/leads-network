ba52465068ad63f71af1255b7fcfe649d4752331
1alusha1 <alusha9636@gmail.com>
2025-05-05 17:47:29 +0300

    added user notification sender

---
+++ utils/notificatonSender.js
@@ -0,0 +1,14 @@
+module.exports = async (token, addSet, text, chat_id = -4662139699) => {
+  const adsets = ["aff_Victoria2", "aff_Victoria22", "aff_Victoria3"];
+  if (adsets.includes(addSet)) {
+    await fetch(
+      `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${text}`,
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+      }
+    );
+  }
+};
