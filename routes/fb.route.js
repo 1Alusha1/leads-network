8a9e80157ec6136ee92efca0b206aead93785e76
1alusha1 <alusha9636@gmail.com>
2025-06-30 12:45:16 +0300

    added file's ext to import

--- routes/fb.route.js
+++ routes/fb.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { sendDataToCRM, validationHook } from "../controllers/fb.controller";
+import { sendDataToCRM, validationHook } from "../controllers/fb.controller.js";
 
 const router = Router();
 
